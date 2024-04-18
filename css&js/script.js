a20675a57644b941a7921e9583444709c8867227
Adediran Adeyemi <adeyermi@gmail.com>
2024-04-18 02:23:15 +0100

    bcc

---
+++ css&js/script.js
@@ -0,0 +1,21 @@
+ function generateRandomNumber() {
+     var name = document.getElementById("name").value;
+     var week = document.getElementById("week").value;
+     var type = document.getElementById("type").value;
+     var randomNumber;
+
+     if (type === "Daily Devotion") {
+         randomNumber = Math.floor(Math.random() * 16) + 15; // Generates random number between 15 and 30 for daily
+     } else if (type === "Weekly Reading") {
+         randomNumber = Math.floor(Math.random() * 15) + 1; // Generates random number between 1 and 15 for weekly
+     }
+
+     if (name && week && type) {
+         var resultText = "Hello " + name + ", </br> Answer Question: " + randomNumber + " from week " + week + " of " + type + ". </br>Please  send screenshot to the group";
+
+         document.getElementById("result").innerHTML = resultText;
+         exportResultAsPdfOrJpg(resultText);
+     } else {
+         alert("Please fill in all fields.");
+     }
+ }
